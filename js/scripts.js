47924e120b848603844bf43179d9ba7f5e55db6f
Brett New and Charles Ewel <charles@>
2016-06-08 14:19:37 -0700

    It will now translate sentences and phrases into pig latin.

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,14 @@ $(document).ready(function() {
   $("form#pig-latin").submit(function(event) {
     event.preventDefault();
     var phrase = $("input#phrase").val();
-    var result = pigLatin(phrase);
+    var result = pigLatin(phrase+" ");
     $("#result").text(result);
   });
 });
 
 var pigLatin = function(phrase){
-  var vowel = /([^aeiou][^aeiouy]*)(\w*)/i
-  return phrase.replace(vowel, "$2$1ay");
+  var vowel = /(qu|[^aeiou]{0,1}(?:qu|[^aeiouy]*))(\w*)(\W*)(\s)/ig
+  return phrase.replace(vowel, "$2$1ay$3$4");
 };
 
 // var pigLatin = function(phrase){
